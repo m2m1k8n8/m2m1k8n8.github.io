2f2cefebc03a56dbcce6941f13d003ba28131b71
Mehrbod Mohammad Khani Nejad <m2m1k8n8@gmail.com>
2024-09-15 19:05:19 +0330

    Create JavaScript and JQuery

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+    $('#save-task').on('click', function() {
+        const taskName = $('#task-name').val().trim();
+        if (taskName === '') {
+            alert('Please enter a task name.');
+            return;
+        }
 
-})
+        const taskItem = $('<div class="col-md-6 task-item"></div>').append(
+            $('<span></span>').text(taskName),
+            $('<button>Delete</button>').on('click', function() {
+                $(this).parent().remove();
+            })
+        );
+
+        $('#task-list').append(taskItem);
+        $('#task-name').val('');
+        $('#taskModal').modal('hide');
+    });
+});
